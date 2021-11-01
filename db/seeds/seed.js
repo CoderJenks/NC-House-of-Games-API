bbda60f2029b091f727bba64a9a1043eedadf3c0
Sam <89224927+CoderJenks@users.noreply.github.com>
2021-11-01 11:14:55 +0000

    decided order of table creation for seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,18 @@
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. create tables
+
+  // Order of tables:
+  //  Categories - not reliant on other tables
+  //  Comments - requires an author which is linked to users.username
+  //    and a review_id which is linked to reviews.review_id.
+  //  Reviews - requires an owner which is linked to users.username
+  //    and a category which is linked to categories.slug
+  //  Users - not reliant on other tables
+  //  *Create tables and insert data in the following order:
+  //  Users -> Categories -> Reviews -> Comments
+
+  return dropTable()
   // 2. insert data
 };
 
