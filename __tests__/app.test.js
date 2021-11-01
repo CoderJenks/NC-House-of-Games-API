6d0193bdc8412f09aac0bbb0348e298b9ba185aa
Sam <89224927+CoderJenks@users.noreply.github.com>
2021-11-01 16:23:49 +0000

    GET /api/categories first test fails

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,13 @@ const app = require("../app.js")
 
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const seed = require('../db/seeds/seed.js');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("GET /api/categories", () => {
-    testData("status 200, responds with an array of games", () => {
+    test("status 200, responds with an array of games", () => {
         return request(app)
         .get("/api/games")
         .expect(200)
