9466bc39e9bd97e10db676b1926d9fcb8f380b1b
Sam <89224927+CoderJenks@users.noreply.github.com>
2021-11-29 23:55:33 +0000

    added JSON describing all the available endpoints for GET /api

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,11 +1,10 @@
 const apiRouter = require("express").Router();
-const categoriesRouter = require('./categories-router.js');
+const getApi = require ("../controllers/api-controller");
+const categoriesRouter = require("./categories-router.js");
 const reviewsRouter = require("./reviews-router.js");
-const commentsRouter = require("./comments-router.js")
+const commentsRouter = require("./comments-router.js");
 
-apiRouter.get("/", (req, res) => {
-    res.status(200).send({ msg: "Welcome to the House of Games"})
-});
+apiRouter.get("/", getApi);
 
 apiRouter.use("/categories",categoriesRouter);
 apiRouter.use("/reviews",reviewsRouter);
