c2840b1ee04cad94e0ac05bbd2525d738f008583
Sam <89224927+CoderJenks@users.noreply.github.com>
2021-11-30 00:45:23 +0000

    updated the description of the available endpoints

--- routes/api-router.js
+++ routes/api-router.js
@@ -12,11 +12,11 @@ apiRouter.get("/", (req, res) => {
             "GET /api" : "Responds with a list of available end points",
             "GET /api/reviews" : "Responds with an array of reviews which accepts sort_by, order and category queries",
             "GET /api/reviews/:review_id" : "Responds with a review object corresponding to the review_id given in the endpoint",
-            "PATCH /api/reviews/:review_id" : "Updates the vote count for the review corresponding to the review_id given in the endpoint and returns the updated review",
-            "GET /api/reviews/:review_id/comments" : "Responds with an array of commment objects relating for the corresponding review_id given in the endpoint",
-            "POST /api/reviews/:review_id/comments" : "Adds a comment to the review corresponding to the review_id given in the endpoint and returns the posted comment.  Requires a username and comment body",
+            "PATCH /api/reviews/:review_id" : "Updates the vote count for the review object corresponding to the review_id given in the endpoint.  Returns the updated review",
+            "GET /api/reviews/:review_id/comments" : "Responds with an array of commment objects corresponding to the review_id given in the endpoint",
+            "POST /api/reviews/:review_id/comments" : "Adds a comment to the review corresponding to the review_id given in the endpoint.  Returns the posted comment.  Requires a username and comment body",
             "GET /api/categories" : "Responds with an array of categories",
-            "DELETE /api/comments/:comment_id" : "Deletes a comment from the database for the comment corresponding to the comment_id given in the endpoint"
+            "DELETE /api/comments/:comment_id" : "Deletes the comment object which corresponds to the comment_id in the endpoint"
         }
     })
 });
